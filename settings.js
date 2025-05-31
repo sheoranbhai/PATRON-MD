1ae63f4d93c96c68293748071d18509c49d145b0
sheoranbhai <sheoranbhai91@gmail.com>
2025-05-31 08:01:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,14 +1,14 @@
 //auto presence update
-global.bankname = "MONIEPOINT"
+global.bankname = ""
 
-global.banknumber = "8133729715"
+global.banknumber = ""
 
-global.bankowner = "FADARE"
+global.bankowner = ""
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
 global.wlcm = true
-global.autoblockmorroco = true //auto block 212 (true to on, false to off)
+global.autoblockmorroco = false //auto block 212 (true to on, false to off)
 global.autokickmorroco = true //auto kick 212 (true to on, false to off) 
 global.antispam = false//auto kick spammer (true to on, false to off)
 global.xprefix = '.'
